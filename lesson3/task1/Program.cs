6a16ab5250c98bd6a0fb02beb7a0b20695b49264
Nezerdrake <nezerdrake@gmail.com>
2024-04-15 02:58:02 +0300

    added task1 in lesson3

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    double denominator = c + d;
+    double result = numinator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
